463f613e5ddac7b542a033649887f858f4028921
filip-koska <filip2k4@gmail.com>
2025-02-18 18:14:10 +0100

    Update ZlecenieCzasowe.java

--- src/zlecenia/ZlecenieCzasowe.java
+++ src/zlecenia/ZlecenieCzasowe.java
@@ -3,6 +3,7 @@ package zlecenia;
 import inwestorzy.Inwestor;
 import symulacja.DaneSymulacji;
 
+// Fixed-expiration order
 public class ZlecenieCzasowe extends Zlecenie {
     public ZlecenieCzasowe(TypZlecenia typZlecenia, String spółka, int liczbaAkcji, int limitCeny,
                            int turaZłożenia, int kolejnośćZłożenia, int ostatniaTura, Inwestor inwestor) {
