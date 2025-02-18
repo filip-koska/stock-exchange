6a3f6dca1ed290084e6d7aedd6553b31450464f9
filip-koska <filip2k4@gmail.com>
2025-02-18 18:13:32 +0100

    Update TerminZlecenia.java

--- src/zlecenia/TerminZlecenia.java
+++ src/zlecenia/TerminZlecenia.java
@@ -2,7 +2,7 @@ package zlecenia;
 
 // Enum for order expiration types
 public enum TerminZlecenia {
-    NA,
-    BT,
-    CZ;
+    NA, // immediate
+    BT, // non-expiring
+    CZ; // fixed-expiration
 }
