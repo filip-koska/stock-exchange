9a53549c3a9ab47d1678f75fc7815cfd859b69e8
filip-koska <filip2k4@gmail.com>
2025-02-18 18:02:09 +0100

    Update ParaZleceń.java

--- src/użytkowe/ParaZleceń.java
+++ src/użytkowe/ParaZleceń.java
@@ -3,15 +3,22 @@ package użytkowe;
 import zlecenia.TypZlecenia;
 import zlecenia.Zlecenie;
 
+// Simple pair of orders
 public class ParaZleceń {
+
+    // buy order
     private Zlecenie kupno;
+    // sell order
     private Zlecenie sprzedaż;
 
-
+    // Technicalities
+    
     public ParaZleceń(Zlecenie kupno, Zlecenie sprzedaż) {
         this.kupno = kupno;
         this.sprzedaż = sprzedaż;
     }
+
+    // Operations
 
     public Zlecenie kupno() {
         return this.kupno;
