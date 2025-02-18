ee93eb8a092a01b361e6f7743f0e76937f7daba8
filip-koska <filip2k4@gmail.com>
2025-02-18 18:01:26 +0100

    Update ParaSpółkaLiczba.java

--- src/użytkowe/ParaSpółkaLiczba.java
+++ src/użytkowe/ParaSpółkaLiczba.java
@@ -1,13 +1,22 @@
 package użytkowe;
 
+// Simple <company, number of shares> pair structure
 public class ParaSpółkaLiczba {
+    // Data
+
+    // company
     private String spółka;
+    // number of shares
     private int liczbaAkcji;
+
+    // Technicalities
 
     public ParaSpółkaLiczba(String spółka, int liczbaAkcji) {
         this.spółka = spółka;
         this.liczbaAkcji = liczbaAkcji;
     }
+
+    // Operations
 
     public String spółka() {
         return this.spółka;
