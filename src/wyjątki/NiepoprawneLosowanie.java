7b6e4f3d142c986acba187a869d1b61949e72bdb
filip-koska <filip2k4@gmail.com>
2025-02-18 18:05:36 +0100

    Update NiepoprawneLosowanie.java

--- src/wyjątki/NiepoprawneLosowanie.java
+++ src/wyjątki/NiepoprawneLosowanie.java
@@ -1,5 +1,6 @@
 package wyjątki;
 
+// Invalid Random Number Exception
 public class NiepoprawneLosowanie extends RuntimeException {
     public NiepoprawneLosowanie() {
         super("Niepoprawne granice losowania");
