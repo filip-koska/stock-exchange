6ed87f6cf53e707deeaaed27aff050d688c92fff
filip-koska <filip2k4@gmail.com>
2025-02-18 18:05:24 +0100

    Update NiepoprawnaTransakcja.java

--- src/wyjątki/NiepoprawnaTransakcja.java
+++ src/wyjątki/NiepoprawnaTransakcja.java
@@ -1,5 +1,6 @@
 package wyjątki;
 
+// Invalid Transaction Exception
 public class NiepoprawnaTransakcja extends RuntimeException {
     public NiepoprawnaTransakcja() {
         super("Niepoprawna transakcja - nielegalny stan inwestora");
