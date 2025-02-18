ffb0ca7497c0fff73f8f6020e7c88f0d7ce6e86b
filip-koska <filip2k4@gmail.com>
2025-02-18 18:05:46 +0100

    Update NiepoprawneZlecenie.java

--- src/wyjątki/NiepoprawneZlecenie.java
+++ src/wyjątki/NiepoprawneZlecenie.java
@@ -1,5 +1,6 @@
 package wyjątki;
 
+// Invalid Order Exception
 public class NiepoprawneZlecenie extends RuntimeException {
     public NiepoprawneZlecenie() {
         super("Niepoprawne dane zlecenia");
