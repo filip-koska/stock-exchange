379143a386ff72059e0bcca32480f761806e5f9e
filip-koska <filip2k4@gmail.com>
2025-02-18 18:00:43 +0100

    Update ParaKolejekZleceń.java

--- src/użytkowe/ParaKolejekZleceń.java
+++ src/użytkowe/ParaKolejekZleceń.java
@@ -7,14 +7,15 @@ import symulacja.Symulacja;
 import zlecenia.TypZlecenia;
 import zlecenia.Zlecenie;
 
+// Pair of order queues
 public class ParaKolejekZleceń implements StrukturaKolejekZleceń {
-    // konwencja: trzymamy tablicę dwuelementową kolejek priorytetowych
-    // pierwsza odpowiada za zlecenia kupna, druga za zlecenia sprzedaży
 
-    // Dane
+    // Data
+
+    // array of size 2 of order queues
     private final ArrayList<PriorityQueue<Zlecenie>> zlecenia;
 
-    // Techniczne
+    // Technicalities
 
     public ParaKolejekZleceń() {
         this.zlecenia = new ArrayList<>();
@@ -22,27 +23,29 @@ public class ParaKolejekZleceń implements StrukturaKolejekZleceń {
             this.zlecenia.add(new PriorityQueue<>());
     }
 
-    // Operacje
+    // Operations
 
-    // wstaw zlecenie do odpowiadającej mu kolejki
+    // Enqueue order
     public void wstaw(Zlecenie z) {
         this.zlecenia.get(z.typZlecenia().ordinal()).add(z);
     }
-
-    // spróbuj obsłużyć parę zleceń z wierzchu pary kolejek
+    
+    // Try to match a pair of orders from the top of the order queues
     public ParaZleceń zdejmijParę() {
         return new ParaZleceń(this.zlecenia.get(0).poll(), this.zlecenia.get(1).poll());
     }
 
+    // isEmpty for buy orders queue
     public boolean kupnoPusta() {
         return this.zlecenia.get(TypZlecenia.KUPNO.ordinal()).isEmpty();
     }
 
+    // isEmpty for sell orders queue
     public boolean sprzedażPusta() {
         return this.zlecenia.get(TypZlecenia.SPRZEDAŻ.ordinal()).isEmpty();
     }
 
-    // przerzuca wszystkie elementy pary kolejek na stos zleceń niezrealizowanych w całości
+    // Moves all uncompleted orders to the uncompleted orders stack
     public void przerzućWszystkie(ArrayList<Zlecenie> niezrealizowane) {
         for (int i = 0; i < 2; ++i)
             while (!this.zlecenia.get(i).isEmpty())
