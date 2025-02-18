19415c813282593044a0630d13e75e0e28fb0ad7
filip-koska <filip2k4@gmail.com>
2025-02-18 18:05:58 +0100

    Update RóżneTypy.java

--- src/wyjątki/RóżneTypy.java
+++ src/wyjątki/RóżneTypy.java
@@ -1,5 +1,6 @@
 package wyjątki;
 
+// Incompatible Types Exception
 public class RóżneTypy extends RuntimeException {
     public RóżneTypy() {
         super("Próba porównania zleceń różnych typów");
