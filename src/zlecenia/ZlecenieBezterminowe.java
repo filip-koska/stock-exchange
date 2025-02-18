11785d814aaac9b1055518e095a06e352693f4ae
filip-koska <filip2k4@gmail.com>
2025-02-18 18:13:52 +0100

    Update ZlecenieBezterminowe.java

--- src/zlecenia/ZlecenieBezterminowe.java
+++ src/zlecenia/ZlecenieBezterminowe.java
@@ -3,7 +3,7 @@ package zlecenia;
 import inwestorzy.Inwestor;
 import symulacja.DaneSymulacji;
 
-// Non-expiring contract
+// Non-expiring order
 public class ZlecenieBezterminowe extends Zlecenie{
     public ZlecenieBezterminowe(TypZlecenia typZlecenia, String spółka, int liczbaAkcji, int limitCeny,
                                 int turaZłożenia, int kolejnośćZłożenia, int ostatniaTura, Inwestor inwestor) {
